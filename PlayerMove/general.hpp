171d66c55ab6025d18445e656c0b6ed8a0965604
pupungeggang2 <kyongkyong2892@gmail.com>
2025-02-23 21:12:47 +0900

    Start PlayerMove

---
+++ PlayerMove/general.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <glad/glad.h>
+#include <SDL2/SDL.h>
+
+extern "C" {
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
+}
---
+++ PlayerMove/var.cpp
@@ -0,0 +1,7 @@
+#include "var.hpp"
+
+SDL_Window *window;
+SDL_GLContext context;
+
+bool running = true;
+unsigned int a = 5;
