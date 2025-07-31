6c1a5f53c3c99336d70959f946edbb9b9e4c0d13
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-31 11:03:56 +0900

    Start

--- PlayerMove/var.cpp
+++
@@ -1,7 +0,0 @@
-#include "var.hpp"
-
-SDL_Window *window;
-SDL_GLContext context;
-
-bool running = true;
-unsigned int a = 5;
--- PlayerMove/var.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "general.hpp"
-
-extern SDL_Window *window;
-extern SDL_GLContext context;
-
-extern bool running;
-extern unsigned int a;
