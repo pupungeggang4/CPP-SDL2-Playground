412aed000564a2c4e4c216cb3190d8263d132e28
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-31 11:37:31 +0900

    Start

---
+++ gl3d/main.cpp
@@ -0,0 +1,5 @@
+#include "script/general.hpp"
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
---
+++ gl3d/script/general.hpp
@@ -0,0 +1,14 @@
+// Headers
+// STL Headers
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+// Library Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
