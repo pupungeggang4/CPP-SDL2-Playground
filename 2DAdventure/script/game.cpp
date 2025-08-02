f6c0cc14bc0627dd1a72dedbdafda12ae9db46ab
pupungeggang2 <kyongkyong2892@gmail.com>
2025-08-02 23:40:03 +0900

    Start

--- 2DAdventure/script/game.cpp
+++ 2DAdventure/script/game.cpp
@@ -10,8 +10,10 @@ void Game::gameSDLInit() {
 
     #ifdef __LINUX__
     window = SDL_CreateWindow("2D Adventure Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280 * 1.5, 720 * 1.5, SDL_WINDOW_SHOWN);
+    scale = 1.5;
     #else
     window = SDL_CreateWindow("2D Adventure Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+    scale = 1.5;
     #endif
     surface = SDL_GetWindowSurface(window);
 }
@@ -20,6 +22,7 @@ void Game::run() {
     SDL_Event e;
     while (running) {
         SDL_FillRect(surface, NULL, 0xFFFFFFFF);
+        SDL_FillRect(surface, {0.0, 0.0, 1280.0, 720}, 0x00000000);
         SDL_UpdateWindowSurface(window);
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
